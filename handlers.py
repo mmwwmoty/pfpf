7e2f2ebf27dff13441713ab5f248b80c8c0e3e01
mwmwmoty <145709969+mmwwmoty@users.noreply.github.com>
2024-05-19 14:54:40 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -13,6 +13,7 @@ async def process_callback_reply(callback_query: types.CallbackQuery, state: FSM
     await bot.send_message(callback_query.from_user.id, "🚀 Здесь можно отправить <b>анонимное сообщение</b> человеку, который опубликовал эту ссылку\n\n🖊 <b>Напишите сюда всё, что хотите ему передать</b>, и через несколько секунд он получит ваше сообщение, но не будет знать от кого\n\nОтправить можно фото, видео, 💬 текст, 🔊 голосовые, 📷видеосообщения (кружки), а также ✨ стикеры", parse_mode="HTML", reply_markup=markup)
     await Form.anonymous_reply.set()
 
+
 @dp.message_handler(content_types=['photo'], state=Form.anonymous_message)
 async def process_anonymous_photo(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
@@ -26,18 +27,22 @@ async def process_anonymous_photo(message: types.Message, state: FSMContext):
     await conn.commit()
     await conn.close()
 
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
+    again_markup.add(again_button)
+
+    # Отправка сообщения отправителю
+    await message.answer("Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
+
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     await bot.send_photo(chat_id=recipient_id, photo=message.photo[-1].file_id, caption="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, parse_mode="HTML")
-
-    again_markup = InlineKeyboardMarkup()
-    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
-    again_markup.add(again_button)
-
-    await bot.send_message(message.from_user.id, "Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
     await state.finish()
+
 
 @dp.message_handler(content_types=['photo'], state=Form.anonymous_reply)
 async def process_anonymous_reply_photo(message: types.Message, state: FSMContext):
@@ -52,18 +57,22 @@ async def process_anonymous_reply_photo(message: types.Message, state: FSMContex
     await conn.commit()
     await conn.close()
 
-    reply_markup = InlineKeyboardMarkup()
-    reply_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
-    reply_markup.add(reply_button)
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
+    again_markup.add(again_button)
 
-    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=reply_markup)
+    # Отправка сообщения отправителю
+    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
 
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     await bot.send_photo(sender_id, photo_id, caption="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, parse_mode="HTML")
     await state.finish()
+
 
 @dp.message_handler(content_types=['video'], state=Form.anonymous_message)
 async def process_anonymous_video(message: types.Message, state: FSMContext):
@@ -78,18 +87,22 @@ async def process_anonymous_video(message: types.Message, state: FSMContext):
     await conn.commit()
     await conn.close()
 
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
+    again_markup.add(again_button)
+
+    # Отправка сообщения отправителю
+    await message.answer("Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
+
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     await bot.send_video(chat_id=recipient_id, video=message.video.file_id, caption="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, parse_mode="HTML")
-
-    again_markup = InlineKeyboardMarkup()
-    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
-    again_markup.add(again_button)
-
-    await bot.send_message(message.from_user.id, "Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
     await state.finish()
+
 
 @dp.message_handler(content_types=['video'], state=Form.anonymous_reply)
 async def process_anonymous_reply_video(message: types.Message, state: FSMContext):
@@ -104,18 +117,22 @@ async def process_anonymous_reply_video(message: types.Message, state: FSMContex
     await conn.commit()
     await conn.close()
 
-    reply_markup = InlineKeyboardMarkup()
-    reply_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
-    reply_markup.add(reply_button)
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
+    again_markup.add(again_button)
 
-    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=reply_markup)
+    # Отправка сообщения отправителю
+    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
 
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     await bot.send_video(sender_id, video_id, caption="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, parse_mode="HTML")
     await state.finish()
+
 
 @dp.message_handler(content_types=['voice'], state=Form.anonymous_message)
 async def process_anonymous_voice(message: types.Message, state: FSMContext):
@@ -130,18 +147,22 @@ async def process_anonymous_voice(message: types.Message, state: FSMContext):
     await conn.commit()
     await conn.close()
 
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
+    again_markup.add(again_button)
+
+    # Отправка сообщения отправителю
+    await message.answer("Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
+
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     await bot.send_voice(chat_id=recipient_id, voice=message.voice.file_id, caption="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, parse_mode="HTML")
-
-    again_markup = InlineKeyboardMarkup()
-    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
-    again_markup.add(again_button)
-
-    await bot.send_message(message.from_user.id, "Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
     await state.finish()
+
 
 @dp.message_handler(content_types=['voice'], state=Form.anonymous_reply)
 async def process_anonymous_reply_voice(message: types.Message, state: FSMContext):
@@ -156,18 +177,22 @@ async def process_anonymous_reply_voice(message: types.Message, state: FSMContex
     await conn.commit()
     await conn.close()
 
-    reply_markup = InlineKeyboardMarkup()
-    reply_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
-    reply_markup.add(reply_button)
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
+    again_markup.add(again_button)
 
-    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=reply_markup)
+    # Отправка сообщения отправителю
+    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
 
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     await bot.send_voice(sender_id, voice_id, caption="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, parse_mode="HTML")
     await state.finish()
+
 
 @dp.message_handler(content_types=['video_note'], state=Form.anonymous_message)
 async def process_anonymous_video_note(message: types.Message, state: FSMContext):
@@ -182,19 +207,23 @@ async def process_anonymous_video_note(message: types.Message, state: FSMContext
     await conn.commit()
     await conn.close()
 
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
+    again_markup.add(again_button)
+
+    # Отправка сообщения отправителю
+    await message.answer("Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
+
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     video_note_message = await bot.send_video_note(chat_id=recipient_id, video_note=video_note_id)
     await bot.send_message(chat_id=recipient_id, text="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, reply_to_message_id=video_note_message.message_id, parse_mode="HTML")
-
-    again_markup = InlineKeyboardMarkup()
-    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
-    again_markup.add(again_button)
-
-    await bot.send_message(message.from_user.id, "Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
     await state.finish()
+
 
 @dp.message_handler(content_types=['video_note'], state=Form.anonymous_reply)
 async def process_anonymous_reply_video_note(message: types.Message, state: FSMContext):
@@ -209,20 +238,23 @@ async def process_anonymous_reply_video_note(message: types.Message, state: FSMC
     await conn.commit()
     await conn.close()
 
-    reply_markup = InlineKeyboardMarkup()
-    reply_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
-    reply_markup.add(reply_button)
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
+    again_markup.add(again_button)
 
-    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=reply_markup)
+    # Отправка сообщения отправителю
+    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
 
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     video_note_message = await bot.send_video_note(chat_id=sender_id, video_note=video_note_id)
     await bot.send_message(chat_id=sender_id, text="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, reply_to_message_id=video_note_message.message_id, parse_mode="HTML")
-
     await state.finish()
+
 
 @dp.message_handler(content_types=['sticker'], state=Form.anonymous_message)
 async def process_anonymous_sticker(message: types.Message, state: FSMContext):
@@ -237,19 +269,23 @@ async def process_anonymous_sticker(message: types.Message, state: FSMContext):
     await conn.commit()
     await conn.close()
 
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
+    again_markup.add(again_button)
+
+    # Отправка сообщения отправителю
+    await message.answer("Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
+
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     sticker_message = await bot.send_sticker(chat_id=recipient_id, sticker=message.sticker.file_id)
     await bot.send_message(chat_id=recipient_id, text="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, reply_to_message_id=sticker_message.message_id, parse_mode="HTML")
-
-    again_markup = InlineKeyboardMarkup()
-    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
-    again_markup.add(again_button)
-
-    await bot.send_message(message.from_user.id, "Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
     await state.finish()
+
 
 @dp.message_handler(content_types=['sticker'], state=Form.anonymous_reply)
 async def process_anonymous_reply_sticker(message: types.Message, state: FSMContext):
@@ -264,20 +300,23 @@ async def process_anonymous_reply_sticker(message: types.Message, state: FSMCont
     await conn.commit()
     await conn.close()
 
-    reply_markup = InlineKeyboardMarkup()
-    reply_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
-    reply_markup.add(reply_button)
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
+    again_markup.add(again_button)
 
-    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=reply_markup)
+    # Отправка сообщения отправителю
+    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
 
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     sticker_message = await bot.send_sticker(chat_id=sender_id, sticker=sticker_id)
     await bot.send_message(chat_id=sender_id, text="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, reply_to_message_id=sticker_message.message_id, parse_mode="HTML")
-
     await state.finish()
+
 
 @dp.message_handler(content_types=['animation'], state=Form.anonymous_message)
 async def process_anonymous_gif(message: types.Message, state: FSMContext):
@@ -292,18 +331,22 @@ async def process_anonymous_gif(message: types.Message, state: FSMContext):
     await conn.commit()
     await conn.close()
 
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
+    again_markup.add(again_button)
+
+    # Отправка сообщения отправителю
+    await message.answer("Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
+
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     await bot.send_animation(chat_id=recipient_id, animation=gif_id, caption="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, parse_mode="HTML")
-
-    again_markup = InlineKeyboardMarkup()
-    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{recipient_id}")
-    again_markup.add(again_button)
-
-    await bot.send_message(message.from_user.id, "Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
     await state.finish()
+
 
 @dp.message_handler(content_types=['animation'], state=Form.anonymous_reply)
 async def process_anonymous_reply_gif(message: types.Message, state: FSMContext):
@@ -318,16 +361,18 @@ async def process_anonymous_reply_gif(message: types.Message, state: FSMContext)
     await conn.commit()
     await conn.close()
 
-    reply_markup = InlineKeyboardMarkup()
-    reply_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
-    reply_markup.add(reply_button)
+    # Создание клавиатуры "Написать ещё"
+    again_markup = InlineKeyboardMarkup()
+    again_button = InlineKeyboardButton("📨 Написать ещё", callback_data=f"send_again:{sender_id}")
+    again_markup.add(again_button)
 
-    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=reply_markup)
+    # Отправка сообщения отправителю
+    await message.answer(f"Сообщение отправлено, ожидайте ответ!", reply_markup=again_markup)
 
+    # Отправка сообщения получателю
     reply_markup = InlineKeyboardMarkup()
     reply_button = InlineKeyboardButton("✏ Ответить", callback_data=f"reply:{message.from_user.id}")
     reply_markup.add(reply_button)
 
     await bot.send_animation(sender_id, gif_id, caption="<b>🔔 У тебя новое сообщение!</b>\n\n", reply_markup=reply_markup, parse_mode="HTML")
     await state.finish()
-
